bf8ee69939828287fbe37ce856b0048336dd98a6
tinybug-m <arshiarnjbar@gmail.com>
2026-06-10 14:19:01 +0330

    feat: make an empty TopNavBar component

---
+++ src/components/molecules/TopNavBar.tsx
@@ -0,0 +1,18 @@
+// TODO : UI
+// TODO : Function
+
+import Button from '@/src/components/atoms/Button'
+
+const TopNavBar = () => {
+  return (
+    <div className="h-10 w-full flex">
+      <div className="flex-1">
+        <Button type='reset'>Eplore</Button>
+      </div>
+      <div className="flex-1">2</div>
+      <div className="">3</div>
+    </div>
+  )
+}
+
+export default TopNavBar
