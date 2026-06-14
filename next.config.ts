582c75ba9d17dbc13b374c8548d6889fe32acc0d
tinyBug <arshiarnjbar@gmail.com>
2026-06-14 13:45:18 +0330

    feat: fix alowedDevOrigins in local network (everyday i have a new ip in my local netwrok)

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,30 @@
 import type { NextConfig } from 'next'
+import os from 'os'
+
+const getLocalIPs = () => {
+  const interfaces = os.networkInterfaces()
+  const ips: string[] = []
+
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name] || []) {
+      // Filter for IPv4 and skip internal loopback addresses
+      if (iface.family === 'IPv4' && !iface.internal) {
+        ips.push(iface.address)
+      }
+    }
+  }
+  return ips
+}
+
+const localIPs = getLocalIPs()
+console.log('Allowed Dev Origins:', localIPs)
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // experimental: {
-  //   turbo: false,
-  // },
-  allowedDevOrigins: ['10.182.109.154'],
+  allowedDevOrigins: [
+    'localhost',
+    '127.0.0.1',
+    ...localIPs
+  ],
 }
 
 export default nextConfig
