0c335e7ff5417c863f003507e434ea6a1997cc8b
tinyBug <arshiarnjbar@gmail.com>
2026-06-28 02:02:38 +0330

    fix: navbar navigation animation

--- src/components/molecules/TopNavBar.tsx
+++ src/components/molecules/TopNavBar.tsx
@@ -4,10 +4,11 @@
 
 import Button from '@/src/components/atoms/Button'
 import { Search } from '../atoms/Icons'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 const TopNavBar = () => {
   const route = useRouter()
+  const pathName = usePathname()
 
   const goToFeed = () => {
     route.push('/feed')
@@ -15,26 +16,37 @@ const TopNavBar = () => {
   const goToExplore = () => {
     route.push('/reels')
   }
+
+  const isFeedActive = pathName == '/feed'
+  const isReelsActive = pathName == '/reels'
+
   return (
     <div className="h-10 w-full flex">
-      <div className="flex-1">
-        <Button
-          onClick={goToFeed}
-          variant="secondary"
-          className="bg-transparent"
-        >
-          Explore
-        </Button>
+      <div className="flex flex-1 relative">
+        <div
+          className={`absolute bottom-0 bg-neutral-500 z-50 h-1 w-50 transition-all duration-500 ${isReelsActive ? 'left-6/12' : 'left-0'}`}
+        ></div>
+
+        <div className="flex-2">
+          <Button
+            onClick={goToFeed}
+            variant="secondary"
+            className={`bg-transparent ${isFeedActive ? 'text-blue-501 font-bold' : 'text-gray-500'}`}
+          >
+            Explore
+          </Button>
+        </div>
+        <div className="flex-2">
+          <Button
+            onClick={goToExplore}
+            variant="secondary"
+            className={`bg-transparent ${isReelsActive ? 'text-blue-501 font-bold' : 'text-gray-500'}`}
+          >
+            Reels
+          </Button>
+        </div>
       </div>
-      <div className="flex-1">
-        <Button
-          onClick={goToExplore}
-          variant="secondary"
-          className="bg-transparent"
-        >
-          Reels
-        </Button>
-      </div>
+
       <div className="">
         <Button variant="secondary" className="h-full bg-transparent">
           <Search />
